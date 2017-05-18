545b5ed3ac41905649c7dc4ecf5e8500f339227a
JMDeS <jmichaeldeseno@gmail.com>
2017-05-18 08:17:08 -0400

    TembooAccount.h

---
+++ TembooAccount.h
@@ -0,0 +1,3 @@
+#define TEMBOO_ACCOUNT "gate1"  // Your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "myFirstApp"  // Your Temboo app key name
+#define TEMBOO_APP_KEY "vRr6LbmtjiLzhmJmiDsURF0Qpce9ornk"  // Your Temboo app key
